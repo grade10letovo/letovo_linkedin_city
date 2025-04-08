b926de6a49beb983e24fc305e14f309b04b4375e
Artem Konukhov <artemkonuhov008@gmail.com>
2025-04-08 10:34:46 +0300

    Add basic terrain generation and some other things

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -1,89 +1,67 @@
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 
-[ExecuteInEditMode]
 public class TerrainGenerator : MonoBehaviour
 {
-    public Texture2D heightmap; // Текстура высот (грейскейл)
-    public Vector3 terrainSize = new Vector3(1000, 100, 1000); // Размер террейна
-    public float waterLevel = 0.3f; // Уровень воды (0-1)
-    public Material landMaterial; // Материал для суши
-    public Material waterMaterial; // Материал для воды
+    [Header("Terrain Settings")]
+    [Tooltip("Количество выборок по одной стороне heightmap")]
+    public int terrainResolution = 256;
 
-    private GameObject terrainObject; // Ссылка на террейн
-    private GameObject waterObject; // Ссылка на воду
+    [Tooltip("Размер террейна по осям X и Z")]
+    public float terrainSize = 1000f;
 
-    public void GenerateTerrain()
+    [Tooltip("Максимальная высота террейна")]
+    public float terrainHeight = 100f;
+
+    [Tooltip("Скаляр, определяющий «частоту» шума Перлина")]
+    public float noiseScale = 0.01f;
+
+    [Tooltip("Текстура террейна")]
+    public TerrainLayer terrainLayer;
+
+    /// <summary>
+    /// Создаёт и возвращает GameObject террейна с применённым шумом Перлина.
+    /// </summary>
+    public GameObject GenerateTerrain()
     {
-        if (heightmap == null)
+        // Если нужны дополнительные настройки, их можно установить:
+        // newLayer.tileOffset = new Vector2(0, 0);
+        // newLayer.specular = ... (если используете соответствующую конфигурацию материала)
+        // и т.д.
+        // Создаём новый TerrainData
+        TerrainData terrainData = new TerrainData
         {
-            Debug.LogError("Heightmap not set!");
-            return;
-        }
+            heightmapResolution = terrainResolution,
+            size = new Vector3(terrainSize, terrainHeight, terrainSize)
+        };
 
-        // Удаляем старый террейн и воду
-        if (terrainObject != null)
-            DestroyImmediate(terrainObject);
-
-        if (waterObject != null)
-            DestroyImmediate(waterObject);
-
-        // Создаем террейн
-        terrainObject = new GameObject("Generated Terrain");
-        Terrain terrain = terrainObject.AddComponent<Terrain>();
-        terrain.terrainData = new TerrainData();
-
-        // Настройка размеров
-        terrain.terrainData.size = terrainSize;
-
-        // Генерация высот
-        int resolution = heightmap.width; // Разрешение высотной карты
-        terrain.terrainData.heightmapResolution = resolution;
-
-        float[,] heights = new float[resolution, resolution];
-        for (int x = 0; x < resolution; x++)
+        // Генерируем карту высот с использованием шума Перлина
+        float[,] heights = new float[terrainResolution, terrainResolution];
+        for (int z = 0; z < terrainResolution; z++)
         {
-            for (int y = 0; y < resolution; y++)
+            for (int x = 0; x < terrainResolution; x++)
             {
-                // Получаем яркость пикселя (0-1)
-                float height = heightmap.GetPixel(x, y).grayscale;
-                heights[x, y] = height * 100;
-                Debug.Log(height);
+                float noiseX = x * noiseScale;
+                float noiseZ = z * noiseScale;
+                float perlinValue = Mathf.PerlinNoise(noiseX, noiseZ);
+                heights[z, x] = perlinValue; // Значение 0..1
             }
         }
 
-        terrain.terrainData.SetHeights(0, 0, heights);
+        // Применяем сгенерированные высоты к TerrainData
+        terrainData.SetHeights(0, 0, heights);
+        // Получаем текущие слои и добавляем новый
+        List<TerrainLayer> layers = new List<TerrainLayer>(terrainData.terrainLayers);
+        layers.Add(terrainLayer);
 
-        // Устанавливаем материал для суши
-        if (landMaterial != null)
-        {
-            terrain.materialTemplate = landMaterial;
-        }
+        // Назначаем обновлённый массив слоев обратно в TerrainData
+        terrainData.terrainLayers = layers.ToArray();
 
-        // Создаем плоскость воды
-        CreateWater();
-    }
+        // Создаём в сцене сам объект террейна
+        GameObject terrainObject = Terrain.CreateTerrainGameObject(terrainData);
+        terrainObject.name = "ProceduralTerrain";
 
-    private void CreateWater()
-    {
-        waterObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
-        waterObject.name = "Generated Water";
-        waterObject.transform.position = new Vector3(terrainSize.x / 2, waterLevel * terrainSize.y, terrainSize.z / 2);
-        waterObject.transform.localScale = new Vector3(terrainSize.x / 10, 1, terrainSize.z / 10);
-
-        // Устанавливаем материал воды
-        if (waterMaterial != null)
-        {
-            waterObject.GetComponent<Renderer>().material = waterMaterial;
-        }
-    }
-
-    public void ClearTerrain()
-    {
-        if (terrainObject != null)
-            DestroyImmediate(terrainObject);
-
-        if (waterObject != null)
-            DestroyImmediate(waterObject);
+        // Возвращаем созданный объект на случай, если MapGenerator захочет с ним что-то ещё делать
+        return terrainObject;
     }
 }
